98a8baba432022155976a7bb28a568b7cb28797a
Marcus Nogueira <marcusnogueiraa@gmail.com>
2025-01-09 14:53:56 -0300

    build: initial config and project folder structure

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const PORT = 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views")); 
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+    res.render("index", { title: "Dashboard"});
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running in http://localhost:${PORT}`)
+});
